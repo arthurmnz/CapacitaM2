4c3983595629aacf6e32a7f52420a9f3cb9bfde1
Seu Nome <mateus.martinsxd9@gmail.com>
2025-07-07 21:23:04 -0300

    Header por meio de nav + css e Routes

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+    return(
+
+    <div>
+        <h2>Faça Login / Cadastro </h2>
+    </div>
+
+    );
+}
+
+export default Login;
---
+++ src/components/NoPage.jsx
@@ -0,0 +1,7 @@
+export default function NoPage(){
+    return(
+        <>
+        <h1 style={ {backgroundColor: "red", color: "white"} }>Página não encontrada</h1>
+        </>
+    )
+}
