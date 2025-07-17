e36ad939ca2c4766f2545c0c533e7d0de6d75b1a
arthurmnz <arthur.gomemenezes@gmail.com>
2025-07-17 17:41:29 -0300

    ultimos testes0.4

--- src/pages/404Exception/NoPage.jsx
+++ src/pages/404Exception/NoPage.jsx
@@ -2,7 +2,9 @@ import "./NoPage.css";
 export default function NoPage() {
   return (
     <div className="NoPage">
-      <h1>ERROR 404</h1>
+      <div className="errormsg">
+        <h1>ERROR 404</h1>
+      </div>
     </div>
   );
 }
